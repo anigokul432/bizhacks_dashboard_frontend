9b596ecc158000c805cf67e6c55be6d7adb55a7a
Amanda Xu <amandalxu23@gmail.com>
2024-07-24 18:30:08 +0530

    added two columns for graphs

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './MainPage.css';
 import UserCreditScore from './UserCreditScore';
-import CreditHistoryGraph from './CreditHistoryGraph';
+import CreditHistoryGraph from './graphs/CreditHistoryGraph';
 import UserRecommendations from './UserRecommendations';
+import DropdownGraphs from './graphs/DropdownGraphs';
 
 const MainPage = () => {
   return (
     <div className="main-page">
       <div className="centered-container">
         <UserCreditScore />
-        <CreditHistoryGraph />
+        <div className="graphs-container">
+          <div className="graph">
+            <CreditHistoryGraph />
+          </div>
+          <div className="graph">
+            <DropdownGraphs />
+          </div>
+        </div>
         <UserRecommendations />
       </div>
     </div>
